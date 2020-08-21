1b70eb3ed58f2e24f51301d188a4539d49be0891
Caique Moreira <caique_moreira7@hotmail.com>
2020-08-20 23:53:53 -0300

    leitura arquivo, forma ruim

---
+++ src/aplicacao/ProgramFileReadereBuffered.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacao;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author caiqu
+ */
+public class ProgramFileReadereBuffered {
+
+    public static void main(String[] args) {
+        String path = "C:\\Users\\caiqu\\Documents\\curso.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
